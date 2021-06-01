3819adf8b99492f51e39d61374350368359e86ba
Alex Griffith <agriffith@hrcorrosion.com>
2021-06-01 11:09:01 -0500

    end of section 3

--- client-app/src/App.tsx
+++ client-app/src/App.tsx
@@ -2,7 +2,7 @@ import React, { useEffect, useState } from 'react';
 import logo from './logo.svg';
 import './App.css';
 import axios from 'axios';
-import { Header } from 'semantic-ui-react';
+import { Header, List } from 'semantic-ui-react';
 
 function App() {
   const [activities, setActivities] = useState([]);
@@ -15,16 +15,18 @@ function App() {
   }, [])
 
   return (
-    <div className="App">
+    <div>
       <Header as='h2' icon='users' content='Reactvities'/>
 
-        <ul>
-          {activities.map((activity: any) =>(
-            <li key={activity.id}>
+      <List>
+      {activities.map((activity: any) =>(
+            <List.Item key={activity.id}>
               {activity.title}
-            </li>
+            </List.Item>
           ))}
-        </ul>
+      </List>
+
+        
     </div>
   );
 }
